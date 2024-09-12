c19d687283628f539adc313f933ff550fb8dada1
Isakih635 <isakih635@ihmeducation.se>
2024-09-12 16:59:56 +0200

    change code

---
+++ js/app.js
@@ -0,0 +1,7 @@
+console.log("This is line 1.");
+console.log("This is line 2.");
+console.log("This is line 3."); console.log("This is line 4.");
+console.log("This is line 5.");;
+;
+console.log("This is line 6.");
+
